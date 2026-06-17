72197b099ecc94c3026d4d24567ab1414b3a94d0
dshashkin <dshashkin@itorum.ru>
2026-06-17 08:20:09 +0300

    add cart schema

--- app/schemas.py
+++ app/schemas.py
@@ -82,3 +82,33 @@ class Review(ReviewCreate):
     id: Annotated[int, Field()]
     comment_date: Annotated[datetime.datetime, Field(description='Время создания отзыва')]
     is_active: Annotated[bool, Field(description='Активен или нет')]
+
+
+class CartItemBase(BaseModel):
+    product_id: int = Field(description='ID товара')
+    quantity: int = Field(ge=1, description='Количество товара')
+
+
+class CartItemCreate(CartItemBase):
+    pass
+
+
+class CartItemUpdate(BaseModel):
+    quantity: int = Field(..., ge=1, description='Новое количество товара')
+
+
+class CartItem(BaseModel):
+    id: int = Field(..., description="ID позиции корзины")
+    quantity: int = Field(..., ge=1, description="Количество товара")
+    product: Product = Field(..., description="Информация о товаре")
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Cart(BaseModel):
+    user_id: int = Field(..., description="ID пользователя")
+    items: list[CartItem] = Field(default_factory=list, description="Содержимое корзины")
+    total_quantity: int = Field(..., ge=0, description="Общее количество товаров")
+    total_price: Decimal = Field(..., ge=0, description="Общая стоимость товаров")
+
+    model_config = ConfigDict(from_attributes=True)
